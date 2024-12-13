841d06d6d228a0ac4fa489efad084001522b6aeb
AbhipsaMohapatra <abhipsamohaptra270805@gmail.com>
2024-12-13 19:36:16 +0530

    added link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Parse form data
  app.use(cors(
     {
-        "origin": "*",
+        "origin": ["https://i-coder-git-main-abhipsa-mohapatras-projects.vercel.app/"],
         "methods": ["POST","GET"],
         "credentials": true
     }
